cac8460828ae63a12c970d9495c01ffbf10fdfb7
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-08-01 21:46:21 -0700

    edited about me

--- components/about.js
+++ components/about.js
@@ -7,21 +7,24 @@ const About = () => {
       <div className="about-text">
         <h2>Hey! I'm Kiran.</h2>
         <div>
-        I'm a web developer who loves building engaging and intuitive products informed by intelligent user research.
+        I'm a web developer with a user-driven mindset.
         </div>
         <div>
-        I approach development with a product manager mindset, leveraging user insights to generate
-        opportunities and define requirements for new features. Taking a user-first approach, I build
-        applications that are easy to use and intuitive to navigate. Most importantly, I believe
-        in delivering value continuously by shipping fast, quality code 🚀.
+        I design and build applications that are engaging and intuitive to navigate.
+        Most importantly, I believe in delivering value continuously by shipping fast,
+        quality code 🚀.
         </div>
         <div>
-        On top of my technical skills, I have experience in market research, project management,
-        and client relations. I believe the best engineers are multidisciplinary and can think beyond the code.
+        I believe the best engineers are multidisciplinary. On top of my technical skills,
+        I have experience in market research, project management, and client relations and
+        have managed multinational market research projects. Armed with thorough, actionable
+        research findings, my team's Fortune 500 clients have been able to confidently launch
+        products around the world.
         </div>
         <div>
         When I'm not programming, I enjoy baking (I make the world's best sugar-free chocolate
-        chip cookies, guaranteed 🍪) and reading. Some of my favorite books include Ender's Game and The Graveyard Book.
+        chip cookies, guaranteed 🍪) and reading. Some of my favorite books include Ender's
+        Game and The Graveyard Book.
         </div>
       </div>
     </div>
