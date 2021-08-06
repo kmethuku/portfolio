08fbb3252ecfe1b6e6bec9548da6d4929ea44d10
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-08-06 14:54:10 -0700

    changed color scheme

--- components/contact.js
+++ components/contact.js
@@ -4,9 +4,9 @@ const Contact = () => {
   return (
     <div className="contact" id="contact">
       <h2>Contact Me</h2>
-      <h3>I'd love to hear from you! Feel free to reach out on LinkedIn or GitHub
+      <p>I'd love to hear from you! Feel free to reach out on LinkedIn or GitHub
         or shoot me an email at kmethuku@protonmail.com.
-      </h3>
+      </p>
       <div className="contact-icons">
         <a href="https://www.github.com/kmethuku"><img src="/github-purple.png"/></a>
         <a href="https://www.linkedin.com/in/kiranmethuku0831"><img src="/linkedin-purple.png"/></a>
