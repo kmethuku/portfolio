5bb7b1eb842794f256cee94c7d9ce5b9a674ae8d
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-08-07 15:13:02 -0700

    added project contribution details

--- components/projects.js
+++ components/projects.js
@@ -11,6 +11,13 @@ const Projects = () => {
             <a href="https://www.github.com/kmethuku/cardshare-client"><img src="/external-link.png"/></a>
           </div>
           <p className="project-description">A flashcard sharing application made for lifelong learners. Users can create decks, search for available decks, save and study decks using the flashcards interface, and more.</p>
+          <p className="project-description">
+            <p><img src="/right-arrow.png"></img>Designed, developed, and deployed Cardshare as solo developer using React, Node.js, and MongoDB</p>
+            <p><img src="/right-arrow.png"></img>Implemented search logic using JSON data fetched via Google Books API</p>
+            <p><img src="/right-arrow.png"></img>Implemented authentication and authorization with Firebase</p>
+            <p><img src="/right-arrow.png"></img>Crafted a seamless and responsive cross-platform compatible UI using BEM CSS</p>
+            <p><img src="/right-arrow.png"></img>Set up MVC-compliant RESTful API with Express and MongoDB</p>
+          </p>
           <div className="project-tech">
             <img src="/wrench.png" width="32" height="auto"/>
             <p>TypeScript, React, Next.js, Firebase, Express, MongoDB, Jest, SuperTest</p>
@@ -27,6 +34,13 @@ const Projects = () => {
             <a href="https://www.github.com/massimobergamin/remotetrainerfrontend"><img src="/external-link.png"/></a>
           </div>
           <p className="project-description">A client management tool for personal trainers. Trainers can assign workout plans and hold virtual training sessions using the video chat interface with integrated exercise timer.</p>
+          <p className="project-description">
+            <p><img src="/right-arrow.png"></img>Designed, developed, and deployed Fitome with a distributed Agile team of 4 developers using React, Redux, Node.js, and PostgreSQL</p>
+            <p><img src="/right-arrow.png"></img>Configured PWA service worker to handle client-side caching and offline access with Google Workbox</p>
+            <p><img src="/right-arrow.png"></img>Implemented state management and handled asynchronous operations with Redux</p>
+            <p><img src="/right-arrow.png"></img>Designed and built an extensive relational database using PostgreSQL with Sequelize</p>
+            <p><img src="/right-arrow.png"></img>Set up MVC-compliant RESTful API optimized to reduce network load</p>
+          </p>
           <div className="project-tech">
             <img src="/wrench.png" width="32" height="auto"/>
             <p>React, Redux, SimplePeer, Socket.io, Google WorkBox, Next.js, Firebase, Express, PostgreSQL</p>
