0ae2be82edd5fd141c85a70f3fcec44fd91524a7
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-08-11 16:56:38 -0700

    updated resume and projects

--- components/projects.js
+++ components/projects.js
@@ -12,11 +12,12 @@ const Projects = () => {
           </div>
           <p className="project-description">A client management tool for personal trainers. Trainers can assign workout plans and hold virtual training sessions using the video chat interface with integrated exercise timer.</p>
           <p className="project-description">
-            <p><img src="/right-arrow.png"></img>Designed, developed, and deployed Fitome with a distributed Agile team of 4 developers using React, Redux, Node.js, and PostgreSQL</p>
+            <p><img src="/right-arrow.png"></img>Created and deployed Fitome with a distributed Agile team of 4 developers</p>
+            <p><img src="/right-arrow.png"></img>Built reusable UI components using React and Hooks with Next.js</p>
             <p><img src="/right-arrow.png"></img>Configured PWA service worker to handle client-side caching and offline access with Google Workbox</p>
-            <p><img src="/right-arrow.png"></img>Implemented state management and handled async operations with Redux</p>
-            <p><img src="/right-arrow.png"></img>Designed and built an extensive relational database using PostgreSQL with Sequelize</p>
-            <p><img src="/right-arrow.png"></img>Set up MVC-compliant RESTful API optimized to reduce network load</p>
+            <p><img src="/right-arrow.png"></img>Implemented state management with Redux and handled asynchronous operations with Redux Thunk</p>
+            <p><img src="/right-arrow.png"></img>Designed and built an extensive relational database using PostgreSQL</p>
+            <p><img src="/right-arrow.png"></img>Set up MVC-compliant REST API with Express optimized to reduce network load</p>
           </p>
           <div className="project-tech">
             <img src="/wrench.png" width="32" height="auto"/>
@@ -35,11 +36,12 @@ const Projects = () => {
           </div>
           <p className="project-description">A flashcard sharing application made for lifelong learners. Users can create decks, search for available decks, save and study decks using the flashcards interface, and more.</p>
           <p className="project-description">
-            <p><img src="/right-arrow.png"></img>Built Cardshare from the ground up as solo developer using React, Node.js, and MongoDB</p>
-            <p><img src="/right-arrow.png"></img>Implemented search logic using JSON data fetched via Google Books API</p>
+            <p><img src="/right-arrow.png"></img>Built Cardshare from the ground up as solo developer</p>
+            <p><img src="/right-arrow.png"></img>Engineered component-based architecture using React and Hooks with Next.js</p>
+            <p><img src="/right-arrow.png"></img>Designed search algorithm to filter JSON data fetched via Google Books API</p>
             <p><img src="/right-arrow.png"></img>Implemented authentication and authorization with Firebase</p>
             <p><img src="/right-arrow.png"></img>Crafted a seamless and responsive cross-platform compatible UI using BEM CSS</p>
-            <p><img src="/right-arrow.png"></img>Set up MVC-compliant RESTful API with Express and MongoDB</p>
+            <p><img src="/right-arrow.png"></img>Set up MVC-compliant REST API with Node.js, Express, and MongoDB</p>
           </p>
           <div className="project-tech">
             <img src="/wrench.png" width="32" height="auto"/>
